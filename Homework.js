e4a35a365145f2948b6694b4a8c54affa1d26054
Milan Lichev <milan.lichev@gmail.com>
2022-09-02 18:56:17 +0200

    Added Homework to c5

---
+++ Homework.js
@@ -0,0 +1,5 @@
+// const daysOfWeek = ["Monday", "Tuesday" ... "Sunday"]
+
+// example 2 should be Dropdown, ( form input to Select)
+// when adding a new Day to the list , 
+// it should no longer be shown in the dropdown
