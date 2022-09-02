69100ec38933ab70d49a12fb4e536f62efd1751b
Milan Lichev <milan.lichev@gmail.com>
2022-09-02 18:59:49 +0200

    Added branch main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import { LifecycleClassComponent } from "./components/LifecycleClassComponent";
-import { LifecycleFuncComponent } from "./components/LifecycleFuncComponent";
-import { LogIn } from './components/LogIn';
-import { useState } from 'react';
-
 const App = () => {
-  const [show, setShow] = useState(true);
 
   return (
-    <div>
-      <button
-      style={{padding:"10px", width:"100%"}}
-        onClick={() => setShow((s) => !s)}
-      >Show/Hide</button>
-
-      {show && <LifecycleClassComponent />}
-
-      {show && <LifecycleFuncComponent />}
-
-      <LogIn />
-    </div>
+    <div></div>
   );
-};
+}
 
-export default App;
+export default App
